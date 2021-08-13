84bd955a7430368ba874725fc10bed8aca50607e
amy_w <amyzefangwagn@gmail.com>
2021-08-13 13:05:10 -0700

    namecard and photocard classes

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,4 +1,13 @@
-package main.ui;
+package ui;
+
+import com.formdev.flatlaf.intellijthemes.FlatArcOrangeIJTheme;
 
 public class Main {
+
+    public Main() {};
+
+    public static void main(String[] args) {
+        FlatArcOrangeIJTheme.install();
+    }
+
 }
